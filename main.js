5dbaa78bc102575daed53f187b769bca078f0383
SamiraRasa <samiraghaderi09@gmail.com>
2021-09-07 18:41:00 +0200

    feat: Create inspiration section

--- main.js
+++ main.js
@@ -81,6 +81,94 @@ function scrollFunction() {
   }
 }
 
+// ------------------------------------------samira----------------------
+const checkIn = document.querySelector('.check-in-container')
 
+const calenderF = function() {
+  document.querySelector('.container-calender').classList.toggle('showCalender')
+}
+checkIn.addEventListener('click', calenderF)
+
+
+
+let country = document.querySelectorAll(".country")
+let btn = document.querySelectorAll(".btn")
+let box = document.querySelector(".country__items")
+btn.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    if (btn[index].classList.contains("active")) {
+      btn[index].classList.remove("active")
+      country[index].style.display = "none"
+      box.classList.remove("move")
+      item.style.fontWeight = "400"
+    } else {
+      item.style.fontWeight = "700"
+      country[index].style.display = "flex"
+      box.classList.add("move")
+
+
+      country.forEach((itemCountry , i) =>{
+        if (i != index){
+          itemCountry.style.display="none"
+          btn[i].classList.remove("active")
+          btn[i].style.fontWeight = "400"
+        }
+      })
+     
+      btn[index].classList.add("active")
+    }
+  })
+})
+
+
+
+function createCalendar(elem, year, month) {
+
+  let mon = month - 1; // months in JS are 0..11, not 1..12
+  let d = new Date(year, mon);
+
+  let table = '<table><tr><th>MO</th><th>TU</th><th>WE</th><th>TH</th><th>FR</th><th>SA</th><th>SU</th></tr><tr>';
+
+  // spaces for the first row
+  // from Monday till the first day of the month
+  // * * * 1  2  3  4
+  for (let i = 0; i < getDay(d); i++) {
+    table += '<td></td>';
+  }
+
+  // <td> with actual dates
+  while (d.getMonth() == mon) {
+    table += '<td>' + d.getDate() + '</td>';
+
+    if (getDay(d) % 7 == 6) { // sunday, last day of week - newline
+      table += '</tr><tr>';
+    }
+
+    d.setDate(d.getDate() + 1);
+  }
+
+  // add spaces after last days of month for the last row
+  // 29 30 31 * * * *
+  if (getDay(d) != 0) {
+    for (let i = getDay(d); i < 7; i++) {
+      table += '<td></td>';
+    }
+  }
+
+  table += '</tr></table>';
+
+  elem.innerHTML = table;
+}
+
+function getDay(date) { // get day number from 0 (monday) to 6 (sunday)
+  let day = date.getDay();
+  if (day == 0) day = 7; // make Sunday (0) the last day
+  return day - 1;
+}
+
+createCalendar(calendar, 2012, 9);
+
+
+// ------------------------------------------samira----------------------
 
 
